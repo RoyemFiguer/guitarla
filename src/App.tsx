2bd776260c3988cb12575da105d99456a69330e3
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-05-15 17:37:37 -0600

    Migrating add to cart function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useReducer } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { useCart } from './hooks/useCart'
+import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const { cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const [state, dispatch] = useReducer(cartReducer, initialState)
+
+  console.log(state)
 
   return (
     <>
@@ -22,11 +27,11 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              {data.map((guitar) => (
+              {state.data.map((guitar) => (
                   <Guitar 
                     key={guitar.id}
                     guitar={guitar}
-                    addToCart={addToCart}
+                    dispatch={dispatch}
                   />
               ))}
               
